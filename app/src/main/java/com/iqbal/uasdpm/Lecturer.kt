dc594609be7a1d642f0ad4cc0672fb470722fd29
unknown <2011500124@mahasiswa.atmaluhur.ac.id>
2022-06-21 19:45:27 +0700

    UAS DPM 2011500124 Mohammad Iqbal

---
+++ app/src/main/java/com/iqbal/uasdpm/Lecturer.kt
@@ -0,0 +1,11 @@
+package com.iqbal.uasdpm
+
+class Lecturer (
+    var nidn: String,
+    var namaDosen: String,
+    var jabatan: String,
+    var golongan: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String
+)
